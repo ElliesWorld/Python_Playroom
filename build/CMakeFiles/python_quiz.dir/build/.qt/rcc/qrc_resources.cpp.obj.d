5e088a563c689d1e4f3d204212c1e8c9cc6957f5
Ellenorvestin <ellenor.vestin@gmail.com>
2025-06-19 11:42:55 +0100

    Remove build directory from Git tracking

--- build/CMakeFiles/python_quiz.dir/build/.qt/rcc/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/python_quiz.dir/build/.qt/rcc/qrc_resources.cpp.obj: \
- C:/Users/ellen/Python_Playroom/build/.qt/rcc/qrc_resources.cpp
--- build/CMakeFiles/python_quiz_autogen_timestamp_deps.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for python_quiz_autogen_timestamp_deps.
